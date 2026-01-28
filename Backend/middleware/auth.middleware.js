c937bfbd0c23b47dae246ab441486860fa320de9
chandra782 <dandumanichandra790@gamil.com>
2026-01-28 10:40:39 +0530

    token verification changed

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -10,7 +10,7 @@ function auth(req, res, next) {
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = process.env.JWT_SECRET;
+    const decoded = jwt.verify(token, "SECRET_KEY");
     req.user = decoded; 
     next();
   } catch (error) {
